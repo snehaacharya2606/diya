40ab8b656c9c94249d1979c09a84b2072d0f6300
snehaacharya2606 <162148362+snehaacharya2606@users.noreply.github.com>
2024-11-02 19:40:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-if (window.innerWidth < 1024) {
-    alert("For the best experience, please view this site in Desktop Mode.");
+function checkScreenSize() {
+    if (window.innerWidth < 1024) {
+        document.getElementById("customAlert").style.display = "block";
+    }
 }
+
+function closeModal() {
+    document.getElementById("customAlert").style.display = "none";
+}
+
+// Run this function when the page loads
+window.onload = checkScreenSize;
